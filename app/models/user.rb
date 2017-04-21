72f91dc97f4ee9566d2ecd83d2e7d6a4001eda00
Tomohiro Ohno <test@test.com>
2017-04-21 00:12:00 +0000

    fav rev1

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,9 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  has_many :favs
+  has_many :microposts, through: :favs, source: :microposts
+  
   
   def follow(other_user)
     unless self == other_user
@@ -31,4 +34,20 @@ class User < ApplicationRecord
     Micropost.where(user_id: self.following_ids + [self.id])
   end
   
+  # targetをファボる
+  def favorite(target_micropost)
+    #一件目を参照、なかったら作る
+    self.favs.find_or_create_by(micropost_id: target_micropost.id)
+  end
+  
+  # アンファボ
+  def unfavorite(other_user)
+    fav = self.favs.find_by(micropost_id: target_micropost.id)
+    fav.destroy if fav
+  end
+  
+  # ファボってる？
+  def favoriting?(target_micropost)
+    self.favs.include?(target_micropost)
+  end
 end
